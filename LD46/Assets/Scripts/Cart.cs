bb36054e9766a5bf934ae18e2696a14790d16454
Adrian Röfer <aroefer@uni-bremen.de>
2020-04-18 21:29:04 +0200

    Added rail-following and rail mesh generation. Mesh generation has a bug and is not being executed.

---
+++ LD46/Assets/Scripts/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cart : MonoBehaviour {
+    float position = 0.0f;
+    public float velocity = 1.0f;
+
+    public Rail rail;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    void Update() {
+        position += velocity * Time.deltaTime;
+
+        SetPathLocation(rail.path_creator.path, position);
+    }
+
+    public void SetPathLocation(PathCreation.VertexPath path, float distance) {
+        Vector3 position = path.GetPointAtDistance(distance);
+        Vector3 dir      = path.GetDirectionAtDistance(distance);
+
+        transform.position = position;
+        transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+    }
+}
