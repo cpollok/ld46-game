92cfd825c736ac7b48c8f704e337e2818bca7260
Christopher Pollok <cpollok@uni-bremen.de>
2020-04-18 14:49:47 +0200

    Add FireInteractor.

---
+++ LD46/Assets/Scripts/FireInteractor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireNotFoundException : Exception {
+    public FireNotFoundException(string fireName) : base("Couldn't find GameObject containing fire of type: " + fireName) { }
+}
+
+public class FireInteractor<T> : MonoBehaviour where T : Fire {
+    protected T fire;
+
+	protected virtual void Awake() {
+        fire = GameObject.FindObjectOfType<T>();
+
+        if (!fire) {
+            throw new FireNotFoundException(typeof(T).Name);
+        }
+    }
+}
