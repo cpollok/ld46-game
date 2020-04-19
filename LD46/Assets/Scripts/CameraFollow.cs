da4beec5876b0f21ebf0df63e7d227b6a104d348
Christopher Pollok <cpollok@uni-bremen.de>
2020-04-19 18:20:07 +0200

    Add camera follow behaviour

---
+++ LD46/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform follow;
+    [SerializeField] private Vector3 positionOffset;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = follow.position + positionOffset;
+    }
+}
